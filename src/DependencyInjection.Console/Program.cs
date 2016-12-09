d1552945f43ce12732928056e83aa76f4aa473f5
Chris Auckland <Chris.Auckland@red-gate.com>
2016-12-09 13:54:41 +0000

    task2: registered remaining dependencies

--- src/DependencyInjection.Console/Program.cs
+++ src/DependencyInjection.Console/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Autofac;
+﻿using Autofac;
 using DependencyInjection.Console.CharacterWriters;
 using DependencyInjection.Console.SquarePainters;
 using NDesk.Options;
@@ -22,47 +21,35 @@ namespace DependencyInjection.Console
                 {"c|colors", value => useColors = value != null},
                 {"w|width=", value => width = int.Parse(value)},
                 {"h|height=", value => height = int.Parse(value)},
-                {"p|pattern=", value => pattern = value}
+                {"p|pattern=", value => pattern = value.ToLower()}
             };
             optionSet.Parse(args);
 
-            BuildContainer();
+            BuildContainer(useColors, pattern);
 
-            var characterWriter = m_Container.ResolveKeyed<ICharacterWriter>(useColors);
-            var patternWriter = new PatternWriter(characterWriter);
-            var squarePainter = GetSquarePainter(pattern);
-            var patternGenerator = new PatternGenerator(squarePainter);
+            var app = m_Container.Resolve<PatternApp>();
 
-            var app = new PatternApp(patternWriter, patternGenerator);
             app.Run(width, height);
         }
 
-        private static ICharacterWriter GetCharacterWriter(bool useColors)
-        {
-            var writer = new AsciiWriter();
-            return useColors ? (ICharacterWriter) new ColorWriter(writer) : writer;
-        }
-
-        private static ISquarePainter GetSquarePainter(string pattern)
-        {
-            switch (pattern)
-            {
-                case "circle":
-                    return new CircleSquarePainter();
-                case "oddeven":
-                    return new OddEvenSquarePainter();
-                case "white":
-                    return new WhiteSquarePainter();
-                default:
-                    throw new ArgumentException($"Pattern '{pattern}' not found!");
-            }
-        }
-
-        private static void BuildContainer()
+        private static void BuildContainer(bool useColors, string pattern)
         {
             var containerBuilder = new ContainerBuilder();
             containerBuilder.Register(c => new AsciiWriter()).Keyed<ICharacterWriter>(false);
             containerBuilder.Register(c => new ColorWriter(c.ResolveKeyed<ICharacterWriter>(false))).Keyed<ICharacterWriter>(true);
+            containerBuilder.Register(c => new PatternWriter(c.ResolveKeyed<ICharacterWriter>(useColors))).AsSelf();
+
+            containerBuilder.Register(c => new CircleSquarePainter()).Keyed<ISquarePainter>("circle");
+            containerBuilder.Register(c => new OddEvenSquarePainter()).Keyed<ISquarePainter>("oddeven");
+            containerBuilder.Register(c => new WhiteSquarePainter()).Keyed<ISquarePainter>("white");
+
+            containerBuilder.Register(c => new PatternGenerator(c.ResolveKeyed<ISquarePainter>(pattern))).AsSelf();
+
+            containerBuilder.Register(
+                c =>
+                    new PatternApp(c.Resolve<PatternWriter>(), c.Resolve<PatternGenerator>()))
+                .AsSelf();
+
             m_Container = containerBuilder.Build();
         }
     }
